0c2ff3259cd5ed1bb3d4fc652888f39d537bf4b9
Tolga-live <76128236+Tolga-live@users.noreply.github.com>
2021-02-23 23:08:36 +0300

    Calculator
    Calculator

---
+++ Calculator.exe/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Calculator.exe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator.exe ");
+            Console.WriteLine("↓↓↓↓↓↓↓↓↓↓");
+            Console.WriteLine(" ");
+            Console.WriteLine(@"Firstly enter your first number then choose mathematical operation
+finally enter your last number");
+            Console.WriteLine("------------------");
+            Console.WriteLine("Press Enter to Calculate ");
+            string input = Console.ReadLine();
+            if (input == ""+"")
+            {
+                Calculator();
+            }
+            
+        }
+
+        public static void Calculator()
+        {
+            Console.Clear();
+            Console.Write("First number: ");
+            float firstNumber = float.Parse(Console.ReadLine());
+            Console.Write("Operation: ");
+            string operation = Console.ReadLine();
+            Console.Write("Second Number: ");
+            float secondNumber = float.Parse(Console.ReadLine());
+            switch (operation)
+            {
+                case "+":
+                    {
+                        Console.WriteLine((float) firstNumber + secondNumber);
+                        break;
+                    }
+                    
+                case "-":
+                    {
+                        Console.WriteLine((float) firstNumber - secondNumber);
+                        break;
+                    }
+                case "/":
+                    {
+                        Console.WriteLine((float) firstNumber / secondNumber);
+                        break;
+                    }
+                case "*":
+                    {
+                        Console.WriteLine((float) firstNumber* secondNumber);
+                        break;
+                    }
+            }
+            Console.WriteLine("Enter to calculate again");
+            string input = Console.ReadLine();
+            if (input == "" + "")
+            {
+                Calculator();
+            }
+
+        }
+    }
+}
